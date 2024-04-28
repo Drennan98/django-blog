64c9475b23f4cac3cd7c9564575f4b0d8fde6d4f
Darragh Drennan <darraghdrennan5@gmail.com>
2024-04-28 11:13:54 +0000

    Add fixtures directory and json file

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Comment
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('titles',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -40,6 +40,15 @@ class Ticket(models.Model):
         related_name="event_tickets"
     )
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"Ticket for {self.ticket_holder}"
