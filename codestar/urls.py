5d6638901bf7b31aee513d15b99fe25d8205a2de
Darragh Drennan <darraghdrennan5@gmail.com>
2024-05-01 16:40:24 +0000

    Add about to urlpatterns

--- codestar/urls.py
+++ codestar/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as blog_views
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
