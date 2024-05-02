ba09b80916eb59a866cba5a0fe0b1dcefc74545c
Darragh Drennan <darraghdrennan5@gmail.com>
2024-05-02 09:12:34 +0000

    Add authentication

--- about/admin.py
+++ about/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import About
-from django_summernote.admin import SumemernoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
 @admin.register(About)
-class AboutAdmin(SumemernoteModelAdmin):
+class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
